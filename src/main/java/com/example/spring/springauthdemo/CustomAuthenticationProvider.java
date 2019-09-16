bebd64fc6fc0829a993d144b752b04efdac9ef63
Deepanjan Das <30810534+deepanjan2k9@users.noreply.github.com>
2019-09-16 18:04:39 -0400

    maven dependencies and custom auth provider

---
+++ src/main/java/com/example/spring/springauthdemo/CustomAuthenticationProvider.java
@@ -0,0 +1,35 @@
+package com.example.spring.springauthdemo;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+@ComponentScan("com.example.spring.springauthdemo")
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+	
+	@Override
+	public Authentication authenticate(Authentication auth) throws AuthenticationException {
+		// TODO Auto-generated method stub
+		String username = auth.getName();
+		String password = auth.getCredentials().toString();
+		
+		if("externaluser".contentEquals(username) && "pass".equals(password)) {
+			return new UsernamePasswordAuthenticationToken(username, password, Collections.emptyList());
+		} else {
+			throw new BadCredentialsException("External system authentication failed. ");	
+		}
+		
+	}
+
+	@Override
+	public boolean supports(Class<?> auth) {
+		// TODO Auto-generated method stub
+		//return false;
+		return auth.equals(UsernamePasswordAuthenticationToken.class);
+	}
+}
